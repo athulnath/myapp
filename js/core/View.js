07c68288959f7980d24f47facad115f5b313e9ca
athulnath <athulnath.t@gmail.com>
2017-09-28 00:17:40 +0530

    [Refactor] moved template base path to variable

--- js/core/View.js
+++ js/core/View.js
@@ -1,11 +1,11 @@
 define(["jquery", "mustache"], function($, Mustache){
 
     function View() {
-
+        this._basepath = "templates/";
     }
 
     View.prototype.renderToApp = function(view, data) {
-        $.get('templates/' + view, function(template) {
+        $.get(this._basepath + view, function(template) {
             $("#app").html(Mustache.render(template, data));
         });
     }
